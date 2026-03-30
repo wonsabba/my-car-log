bdbdb6f064f2411e280e77788e881e47cea694eb
wonsabba <wonsabba@gmail.com>
2026-03-30 15:37:26 +0900

    auto update

--- app/stock/page.tsx
+++ app/stock/page.tsx
@@ -141,7 +141,7 @@ export default function StockPage() {
         <div className="flex justify-between items-center mb-4">
           <div className="flex items-center gap-2">
             <h1 className="text-xl font-black italic tracking-tighter text-blue-500">Stock</h1>
-            <div className="flex gap-1 ml-1">
+            <div className="flex gap-3 ml-2">
               <Link href="/home" className="text-slate-300 hover:text-slate-800 font-black text-1xl tracking-tighter transition-colors">🏠</Link> 
               <Link href="/" className="text-slate-300 hover:text-slate-800 font-black text-1xl tracking-tighter transition-colors">
                 <img src="/GV80.jpg" alt="GV80 Icon" className="w-5 h-5 inline-block mr-1 -mt-1 rounded-md " title="차계부로"/></Link>
